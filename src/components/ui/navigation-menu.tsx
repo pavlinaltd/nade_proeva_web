d1b9a593d9d8b32c2b2d1730f7c3f45fa47c6a7a
Corie Beale <bealecorie@gmail.com>
2025-06-04 13:14:41 -0400

    updated navbar styles for readability

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -30,7 +30,7 @@ const NavigationMenuList = React.forwardRef<
   <NavigationMenuPrimitive.List
     ref={ref}
     className={cn(
-      "group flex flex-1 list-none items-center justify-center space-x-1",
+      "group flex flex-1 list-none items-center justify-center gap-8 space-x-1 text-sm",
       className
     )}
     {...props}
