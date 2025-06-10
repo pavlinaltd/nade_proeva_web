f946991ba297dc36bea0986204f959b7de63fab3
Corie Beale <bealecorie@gmail.com>
2025-06-10 19:42:28 -0400

    reorganized footer content and added additional external links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,99 +12,74 @@ const Footer = () => {
           // className="grid grid-cols-3 gap-8"
           className="flex items-start justify-between"
         >
-          {/* Logo and About Page */}
-          <div className="w-1/2">
-            <img
-              src="/logo_italic.jpg"
-              alt="The Nade Proeva Endowment Fund"
-              className="w-32 h-auto mb-4"
-            />
+          {/* About and Links */}
+          <div className="w-2/5">
             <p className="text-black text-md">
               This site is dedicated to preserving the memory and academic legacy of
               Professor Dr. Nade Proeva, a distinguished Macedonian scholar, historian, researcher, and educator.
             </p>
             <br />
-            <p className="text-black text-md">
-              &copy; All rights reserved. To use portions of the writings featured on this site, you need permission from the copyright holder.
-            </p>
-          </div>
-          
-          {/* Endowment Contact */}
-          {/* <div
-            className="flex flex-col items-start gap-[10px]"
-          >
-            <h3 className="text-xl mb-4 text-black">Endowment Fund Contact</h3>
-            <p className="text-black text-md">
-              Arizona State University<br />
-              X St.<br />
-              X, AZ 11111<br />
-              <a
-                href="mailto:"
-                className="hover:underline"
-              >
-                x@x.edu
-              </a><br />
-              <a
-                href="tel:+11111111111"
-                className="hover:underline"
-              >
-                +1 (111) 111-1111
-              </a>
-            </p>
-            <Button
-              asChild
-              className="bg-burgundy-600 hover:bg-burgundy-700 text-white"
-            >
-              <Link to="/donate">Contribute</Link>
-            </Button>
-          </div> */}
-
-          {/* Links */}
-          <div className="w-1/3">
             <h3 className="text-xl mb-4 text-black">Links</h3>
-            <div className="grid grid-cols-2 w-full">
-              <div>
-                <ul className="space-y-2">
-                  <li>
-                    <Link to="/" className="text-black hover:underline text-md">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/about" className="text-black hover:underline text-md">About</Link>
-                  </li>
-                  <li>
-                    <Link to="/books" className="text-black hover:underline text-md">Books</Link>
-                  </li>
-                  <li>
-                    <Link to="/papers" className="text-black hover:underline text-md">Papers</Link>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <ul className="space-y-2">
-                  <li>
-                    <Link to="/columns" className="text-black hover:underline text-md">Columns</Link>
-                  </li>
-                  <li>
-                    <Link to="/media" className="text-black hover:underline text-md">Media</Link>
-                  </li>
-                  <li>
-                    <Link to="/on-location" className="text-black hover:underline text-md">On Location</Link>
-                  </li>
-                  <li>
-                    <Link to="/testimonials" className="text-black hover:underline text-md">Remembering a Scholar</Link>
-                  </li>
-                </ul>
-              </div>
+            <div className="grid grid-cols-4 w-full">
+              <ul className="space-y-2">
+                <li>
+                  <Link to="/" className="text-black hover:underline text-md">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about" className="text-black hover:underline text-md">About</Link>
+                </li>
+                <li>
+                  <Link to="/books" className="text-black hover:underline text-md">Books</Link>
+                </li>
+              </ul>
+              <ul className="space-y-2">
+                <li>
+                  <Link to="/papers" className="text-black hover:underline text-md">Papers</Link>
+                </li>
+                <li>
+                  <Link to="/columns" className="text-black hover:underline text-md">Columns</Link>
+                </li>
+                <li>
+                  <Link to="/media" className="text-black hover:underline text-md">Media</Link>
+                </li>
+              </ul>
+              <ul className="space-y-2">
+                <li>
+                  <Link to="/on-location" className="text-black hover:underline text-md">On Location</Link>
+                </li>
+                <li>
+                  <Link to="/testimonials" className="text-black hover:underline text-md">Remembering a Scholar</Link>
+                </li>
+                <li>
+                  <Link to="https://ukim.academia.edu/NadeProeva" className="text-black hover:underline text-md">Academia.edu</Link>
+                </li>
+              </ul>
+              <ul className="space-y-2">
+                <li>
+                  <Link to="#" className="text-black hover:underline text-md">University Page</Link>
+                </li>
+              </ul>
             </div>
+          </div>
+
+          {/* Copyright Statement */}
+          <div className="w-2/5">
+            <p className="text-black text-md text-justify">
+              <span className="font-bold">&copy; All rights reserved.</span> The contents of this website, including but not limited to text,
+              images, and other media, are protected by copyright law. No part of these materials may be copied,
+              reproduced, distributed, transmitted, displayed, published, or otherwise used in any form or by any means, 
+              including electronic, mechanical, photocopying, recording, or otherwise, without 
+              the prior written permission of the copyright holder. Unauthorized use is strictly prohibited and may result in legal action.
+            </p>
           </div>
         </div>
         
         <div className="flex justify-between border-t-2 border-amber-800 mt-8 pt-6 text-center text-black text-sm">
           <p>
-            &copy; {currentYear} Nade Proeva Scholarship Endowment. All rights reserved.
+            &copy; {currentYear} <i>Nade Proeva Scholarship Endowment</i>.
           </p>
           <p>
-            Website courtesy of <a href="https://linkedin.com/in/coriebeale" className="hover:underline">Corie Beale</a>
+            Website courtesy of <a href="https://linkedin.com/in/coriebeale" className="font-bold hover:underline">Corie Beale</a>
           </p>
         </div>
       </div>
