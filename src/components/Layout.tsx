c22b34d7802170a5d2f9c70cce215b9d23e517a5
Corie Beale <bealecorie@gmail.com>
2025-06-23 19:47:26 -0400

    white background

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 
 const Layout = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-background">
+    <div className="flex flex-col min-h-screen bg-white">
       <Navbar />
       <main className="flex-grow container mx-auto px-4 py-8 md:py-12 animate-fade-in">
         <Outlet />
