bc203c9ac88a61d8f795c8b2fd0ab6f71ce7babc
Corie Beale <bealecorie@gmail.com>
2025-07-31 11:07:40 -0400

    reformatted audio for youtube links, added first youtube link

--- src/pages/Media.tsx
+++ src/pages/Media.tsx
@@ -15,63 +15,63 @@ const Media = () => {
   const audios = [
     {
       type: "audio",
-      src: "/audio/",
+      src: "https://www.youtube.com/embed/VZTxV5iXBWs",
       english: "Historical Focus",
       original: "Историски Фокус",
       date: "July 19, 2024",
     },
     {
       type: "audio",
-      src: "/audio/TRAGI-VO-VREMETO-06-05-2018.wav",
+      src: "",
       english: "Footprints in Time",
       original: "Траги во Времето",
       date: "May 6, 2018",
     },
     {
       type: "audio",
-      src: "/audio/STUDII-ZA-ANTICKITE-MAKEDONCI.wav",
+      src: "",
       english: "Studies of the Ancient Macedonians",
       original: "Студии за Античките Македонци",
       date: "2011",
     },
     {
       type: "audio",
-      src: "/audio/OD-NASHIOT-NEZABORAV.wav",
+      src: "",
       english: "From Our Unforgettable Past",
       original: "Од Нашиот Незаборав",
       date: "March 3, 2001",
     },
     {
       type: "audio",
-      src: "/audio/",
+      src: "",
       english: "Historical Focus",
       original: "Историски Фокус",
       date: "December 26, 1998",
     },
     {
       type: "audio",
-      src: "/audio/",
+      src: "",
       english: "The Ylirians From Bardilis to Gentij",
       original: "Илирите Од Бардилис до Гентиј",
       date: "June 12, 1997",
     },
     {
       type: "audio",
-      src: "/audio/",
+      src: "",
       english: "Historical Focus",
       original: "Историски Фокус",
       date: "March 23, 1996",
     },
     {
       type: "audio",
-      src: "/audio/",
+      src: "",
       english: "Historical Focus",
       original: "Историски Фокус",
       date: "March 23, 1996",
     },
     {
       type: "audio",
-      src: "/audio/",
+      src: "",
       english: "Traditions and Currents",
       original: "Традиции и Текови",
       date: "March 12, 1991",
@@ -116,7 +116,6 @@ const Media = () => {
         <h3 className="text-2xl font-bold text-center text-burgundy-900">VIDEO</h3>
         <h3 className="text-2xl font-bold text-center text-burgundy-900">AUDIO</h3>
       </div>
-      <hr className="my-8 border-2 rounded-full"/>
       <div className="flex justify-center mt-4 items-start gap-10">
         {/* Video */}
         <div className="flex flex-col gap-10 w-1/2 rounded-md">
@@ -166,29 +165,45 @@ const Media = () => {
           {/* Items */}
           {audios.map((audio, index) => (
             <div
-              key={index} 
-              className="bg-burgundy-600 hover:bg-burgundy-700 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-all duration-200 cursor-pointer h-min"
-              // onClick={() => openLightbox(photo)}
-              // Don't need lightbox for audio
+              key={index}
+              className="bg-gray-200 rounded-lg overflow-hidden shadow-md"
             >
-              <div className="p-4">
-                <a
-                  href='' //{audio.src}
-                  target="_blank"
-                  title=""
-                  className="flex gap-4 items-center"
-                >
-                  <Headphones className="text-white" />
-                  <div className="text-sm flex flex-row text-white w-full justify-between">
-                    <div className="flex-col">
-                      <p /*className="font-bold"*/>{audio.english}</p>
-                      <p>{audio.original}</p>
-                    </div>
-                    <p className="text-burgundy-200 self-center">{audio.date}</p>
+              <div className="aspect-video relative">
+                  <iframe
+                    className="w-full h-full object-cover"
+                    src={audio.src}
+                    title={`${audio.english} - ${audio.original}, ${audio.date}`}
+                  >
+                  </iframe>
+                  <div className="p-4">
+                    <p className="font-droidsans text-black mb-1">{`${audio.english} - ${audio.original}, ${audio.date}`}</p>
                   </div>
-                </a>
               </div>
             </div>
+            // <div
+            //   key={index} 
+            //   className="bg-burgundy-600 hover:bg-burgundy-700 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-all duration-200 cursor-pointer h-min"
+            //   // onClick={() => openLightbox(photo)}
+            //   // Don't need lightbox for audio
+            // >
+            //   <div className="p-4">
+            //     <a
+            //       href='' //{audio.src}
+            //       target="_blank"
+            //       title=""
+            //       className="flex gap-4 items-center"
+            //     >
+            //       <Headphones className="text-white" />
+            //       <div className="text-sm flex flex-row text-white w-full justify-between">
+            //         <div className="flex-col">
+            //           <p /*className="font-bold"*/>{audio.english}</p>
+            //           <p>{audio.original}</p>
+            //         </div>
+            //         <p className="text-burgundy-200 self-center">{audio.date}</p>
+            //       </div>
+            //     </a>
+            //   </div>
+            // </div>
           ))}
         </div>
       </div>
