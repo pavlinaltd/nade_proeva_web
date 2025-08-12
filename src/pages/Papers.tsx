66a3fa330d20b0ac264bf74191e966cc571134c0
Corie Beale <bealecorie@gmail.com>
2025-08-12 09:27:06 -0400

    ordered papers by date

--- src/pages/Papers.tsx
+++ src/pages/Papers.tsx
@@ -29,31 +29,23 @@ const Papers = () => {
       abstract: "This paper explores the connection between the ancient belief in eternal life, as depicted in the myth of Philemon and Baucis, and its reflection in Roman-era funerary practices in Upper Macedonia."
     },
     {
-      title: "Who Were the Authors of the Trebenište Culture and the Gold Funeral Masks?",
-      year: "2006",
-      journal: "Macedonian Affairs",
-      url: "https://www.academia.edu/1439976/Nade_Proeva_WHO_WERE_THE_AUTHORS_OF_THE_TREBENIŠTE_CULTURE_AND_THE_GOLD_FUNERAL_MASKS",
-      abstract: "This article investigates the authorship of the Trebenište culture and the gold funeral masks, analyzing archaeological finds and their implications for understanding ancient Macedonian society."
+      title: "100 Years of Trebenište",
+      year: "2019",
+      journal: "Vjesnik za arheologiju i historiju dalmatinsku",
+      url: "https://www.academia.edu/42127116/100_years_of_Trebenishte",
+      abstract: "This paper reviews an exhibition held at the Archaeological Museum in Skopje in 2019, marking the 100th anniversary of the discovery of two gold funeral masks in the Trebeništa necropolis."
     },
     {
-      title: "A Brief Chronology of Historical Events",
-      year: "2009",
-      journal: "Journal of Historical Studies",
-      url: "https://www.academia.edu/6808303/MACEDONIA_A_BRIEF_CHRONOLOGY_OF_HISTORICAL_EVENTS",
-      abstract: "A list of events in chronological order regarding Macedonian history between the Iron Age and April 8th, 1993."
+      title: "On the Names of Thracia and Eastern Macedonia...",
+      year: "2017",
+      url: "https://www.academia.edu/34673739/On_the_Names_of_Thracia_and_Eastern_Macedonia_%CE%9A%CE%A1%CE%91%CE%A4%CE%99%CE%A3%CE%A4%CE%9F%CE%A3_%D0%A1%D0%B1%D0%BE%D1%80%D0%BD%D0%B8%D0%BA_%D0%B2_%D1%87%D0%B5%D1%81%D1%82_%D0%BD%D0%B0_%D0%BF%D1%80%D0%BE%D1%84%D0%B5%D1%81%D0%BE%D1%80_%D0%9F%D0%B5%D1%82%D1%8A%D1%80_%D0%94%D0%B5%D0%BB%D0%B5%D0%B2_%D0%A1%D0%BE%D1%84%D0%B8%D1%8F_2017_%D1%81%D1%82%D1%80_75_82Proeva_2017_PD_body_color",
+      abstract: "This paper examines how ancient authors used the term Thracia, noting the differences between its ethnic and political boundaries. Nade Proeva proposes a distinction between Thracia stricto sensu (narrowly defined) and Thracia lato sensu (broadly defined), arguing that the term often referred to geographical regions, such as coastal or mainland tribes, without necessarily implying ethnic identity, and suggests using the method of Namengebiet to clarify the boundaries of these areas."
     },
     {
       title: "Sur l'Iconographie des Stèles Funéraires du «Type Tikveš» en Macédoine à l'Époque Romaine",
       year: "2013",
       url: "https://www.academia.edu/3750406/SUR_L_ICONOGRAPHIE_DES_ST%C3%88LES_FUN%C3%89RAIRES_DU_TYPE_TIKVE%C5%A0_EN_MAC%C3%89DOINE_%C3%80_L_%C3%89POQUE_ROMAINE",
       abstract: "This paper examines a group of funeral stelae from Upper Macedonia that feature unique motifs like human figures shaped as trees, grapevines, and agricultural tools."
-    },
-    {
-      title: "100 Years of Trebenište",
-      year: "2019",
-      journal: "Vjesnik za arheologiju i historiju dalmatinsku",
-      url: "https://www.academia.edu/42127116/100_years_of_Trebenishte",
-      abstract: "This paper reviews an exhibition held at the Archaeological Museum in Skopje in 2019, marking the 100th anniversary of the discovery of two gold funeral masks in the Trebeništa necropolis."
     },
     {
       title: "Националните митови во современа Европа и негирањето на македонскиот идентитет",
@@ -63,10 +55,18 @@ const Papers = () => {
       abstract: "This paper analyzes the political pressures and blackmail faced by the Republic of Macedonia following its 1991 independence, particularly regarding the dispute over its name. It critiques the role of NATO, the EU, and national myths—especially the revival of ancient Macedonian identity—in shaping the country's international recognition and identity politics."
     },
     {
-      title: "On the Names of Thracia and Eastern Macedonia...",
-      year: "2017",
-      url: "https://www.academia.edu/34673739/On_the_Names_of_Thracia_and_Eastern_Macedonia_%CE%9A%CE%A1%CE%91%CE%A4%CE%99%CE%A3%CE%A4%CE%9F%CE%A3_%D0%A1%D0%B1%D0%BE%D1%80%D0%BD%D0%B8%D0%BA_%D0%B2_%D1%87%D0%B5%D1%81%D1%82_%D0%BD%D0%B0_%D0%BF%D1%80%D0%BE%D1%84%D0%B5%D1%81%D0%BE%D1%80_%D0%9F%D0%B5%D1%82%D1%8A%D1%80_%D0%94%D0%B5%D0%BB%D0%B5%D0%B2_%D0%A1%D0%BE%D1%84%D0%B8%D1%8F_2017_%D1%81%D1%82%D1%80_75_82Proeva_2017_PD_body_color",
-      abstract: "This paper examines how ancient authors used the term Thracia, noting the differences between its ethnic and political boundaries. Nade Proeva proposes a distinction between Thracia stricto sensu (narrowly defined) and Thracia lato sensu (broadly defined), arguing that the term often referred to geographical regions, such as coastal or mainland tribes, without necessarily implying ethnic identity, and suggests using the method of Namengebiet to clarify the boundaries of these areas."
+      title: "Exegi Monumentum Aere Perennius",
+      year: "2010",
+      journal: "Review of the Institute of History and Archaeology RIHA",
+      url: "https://js.ugd.edu.mk/index.php/riha/article/view/5525",
+      abstract: "The paper is written in honor of and in tribute to Nade Proeva's former student, the late Aleksandar Stojcev, a History Ph.D. specializing in military history."
+    },
+    {
+      title: "A Brief Chronology of Historical Events",
+      year: "2009",
+      journal: "Journal of Historical Studies",
+      url: "https://www.academia.edu/6808303/MACEDONIA_A_BRIEF_CHRONOLOGY_OF_HISTORICAL_EVENTS",
+      abstract: "A list of events in chronological order regarding Macedonian history between the Iron Age and April 8th, 1993."
     },
     {
       title: "The Engelanes / Encheleis and the Golden Mask from the Trebenište Culture",
@@ -76,11 +76,11 @@ const Papers = () => {
       abstract: "This passage discusses Professor M. Garašanin's analysis of the golden funerary masks from the Trebenište culture, which he attributed to the Engelanes/Encheleis tribe."
     },
     {
-      title: "Exegi Monumentum Aere Perennius",
-      year: "2010",
-      journal: "Review of the Institute of History and Archaeology RIHA",
-      url: "https://js.ugd.edu.mk/index.php/riha/article/view/5525",
-      abstract: "The paper is written in honor of and in tribute to Nade Proeva's former student, the late Aleksandar Stojcev, a History Ph.D. specializing in military history."
+      title: "Who Were the Authors of the Trebenište Culture and the Gold Funeral Masks?",
+      year: "2006",
+      journal: "Macedonian Affairs",
+      url: "https://www.academia.edu/1439976/Nade_Proeva_WHO_WERE_THE_AUTHORS_OF_THE_TREBENIŠTE_CULTURE_AND_THE_GOLD_FUNERAL_MASKS",
+      abstract: "This article investigates the authorship of the Trebenište culture and the gold funeral masks, analyzing archaeological finds and their implications for understanding ancient Macedonian society."
     },
   ];
 
