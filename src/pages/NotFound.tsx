46e6089483940eee160bd9d2d75d70d94d40b824
Corie Beale <bealecorie@gmail.com>
2025-09-09 22:11:06 -0400

    styled 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,4 +1,3 @@
-
 import { useLocation, Link } from "react-router-dom";
 import { useEffect } from "react";
 import { ArrowLeft } from "lucide-react";
@@ -15,13 +14,13 @@ const NotFound = () => {
   }, [location.pathname]);
 
   return (
-    <div className="min-h-[70vh] flex flex-col items-center justify-center">
-      <h1 className="text-6xl md:text-8xl font-bold text-memorial-900 mb-4">404</h1>
-      <p className="text-xl text-gray-600 mb-8">We couldn't find the page you're looking for</p>
-      <Button asChild className="flex items-center gap-2">
+    <div className="min-h-[70vh] flex flex-col items-center justify-center gap-5">
+      <h1 className="text-6xl md:text-8xl font-bold text-burgundy-900 mb-4">404</h1>
+      <p className="text-xl text-black mb-8">We couldn't find up the page you're looking for.</p>
+      <Button asChild className="flex items-center gap-2 bg-burgundy-700 hover:bg-burgundy-900 text-white">
         <Link to="/">
           <ArrowLeft size={16} />
-          <span>Return to Homepage</span>
+          <span>Back to Home</span>
         </Link>
       </Button>
     </div>
