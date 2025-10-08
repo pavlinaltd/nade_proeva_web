9c2c575799c63ed8e81c2c258cbd713ab6fccd42
Corie Beale <bealecorie@gmail.com>
2025-10-07 20:08:18 -0400

    enlargened text, used new card layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,3 @@
-import { ReactNode } from "react";
-
-interface CardPairProps {
-  img: string;
-  alt: string;
-  text: ReactNode;
-  imgClass?: string;
-}
-const CardPair = ({img, alt, text, imgClass}: CardPairProps) => {
-  return (
-    <div className="flex flex-col sm:flex-row mx-auto w-full justify-center items-center sm:items-start gap-6 bg-gray-100 border border-gray-300 rounded-lg p-5">
-      <img
-        className={`w-[200px] md:self-stretch rounded-lg object-contain bg-white ${imgClass}`}
-        src={img}
-        alt={alt}
-      />
-
-      <div className="text-black text-justify justify-evenly mx-auto p-2">
-        <p>{text}</p>
-      </div>
-    </div>
-  )
-}
-
 const Home = () => {
 
   const medals = [
@@ -40,7 +16,7 @@ const Home = () => {
   ];
 
   return (
-    <div className="animate-fade-in space-y-10">
+    <div className="animate-fade-in space-y-16">
       {/* Hero Section */}
       <section className="relative p-5 md:p-10 lg:p-20 rounded-lg bg-gradient-to-b from-burgundy-700 to-burgundy-800 text-white">
         <div className="flex flex-col md:flex-row justify-center items-center gap-5 md:gap-10">
@@ -74,13 +50,13 @@ const Home = () => {
 
       {/* Introduction Section */}
       <section className="container mx-auto px-4 w-full lg:w-4/5">
-        <div className="flex flex-col gap-4 justify-center items-center text-justify text-gray-700 xl:text-lg">
-          <p className="leading-relaxed">
+        <div className="flex flex-col gap-8 justify-center items-center text-justify text-gray-700">
+          <p className="leading-relaxed lg:text-xl">
             <strong>Professor Dr. Nade Proeva</strong> was a distinguished Macedonian historian and classicist who
             served as a full professor at the Faculty of Philosophy, Department of History, at Ss.
             Cyril and Methodius University in Skopje, Macedonia from 1984 until her retirement in 2014.
           </p>
-          <p className="leading-relaxed">
+          <p className="leading-relaxed lg:text-xl">
             Born on September 9, 1949, in the town of Resen, in the Socialist Republic of Macedonia
             (then part of the SFR Yugoslavia), Nade Proeva pursued her higher education at the
             University of Belgrade, earning a degree in Classical Archaeology in 1973.
@@ -89,7 +65,7 @@ const Home = () => {
             Her academic journey culminated in the successful defense of her doctoral dissertation,
             <i>The Influence of Settlers on the Development of Cultural Conditions in the Roman Province of Macedonia</i>, in 1992.
           </p>
-          <p className="leading-relaxed">
+          <p className="leading-relaxed lg:text-xl">
             Renowned for her intellectual rigor and unwavering commitment to historical truth,
             Professor Proeva devoted her scholarly career to the study of ancient Macedonia.
             Her research left a profound and lasting impact on the field, earning her a respected place
@@ -109,7 +85,7 @@ const Home = () => {
               src={medal.src}
               alt={`${medal.title} Certificate and Medal`}
               title={medal.title}
-              className="rounded-lg w-full sm:w-2/3 lg:w-full bg-gradient-to-b to-yellow-600 from-yellow-300 p-3"
+              className="rounded-lg w-full sm:w-2/3 lg:w-full"
             />
           ))}
         </div>
@@ -130,35 +106,44 @@ const Home = () => {
       </section>
 
       {/* Combined */}
-      <section className="container flex flex-col justify-between gap-5 md:gap-10 mx-auto px-4 w-full lg:w-4/5 xl:w-3/5">
+      <section className="container flex flex-col justify-between gap-10 md:gap-20 mx-auto px-4 w-full lg:w-2/3 xl:w-4/5">
         {/* Book Feature Section */}
-        <CardPair
-          img={"/images/from-her-lectures.jpg"}
-          alt={"From Her Lectures: Our Words Book Cover"}
-          text={
-            <>This volume is a tribute by six former students of Professor Dr. Nade Proeva, who came together to honor her legacy.
-            It brings together their writings on Macedonian history, spanning archaeology, ethnology, anthropology, mythology, and religion from antiquity to the present.
-            Through these pages, they celebrate their teacher's lasting influence and honor her lifelong devotion to truth, scholarship, and her beloved homeland, Macedonia.
-            </>
-          }
-        />
+        <div className="flex flex-col sm:flex-row mx-auto w-full xl:w-2/3 justify-center items-center sm:items-start gap-6 rounded-lg">
+          <img
+            className={`w-[250px] md:self-stretch rounded-lg object-contain bg-white`}
+            src="/images/from-her-lectures.jpg"
+            alt="From Her Lectures: Our Words Book Cover"
+          />
+
+          <div className="text-black text-justify justify-evenly mx-auto p-2">
+            <p className="lg:text-xl">
+              This volume is a tribute by six former students of Professor Dr. Nade Proeva, who came together to honor her legacy.
+              It brings together their writings on Macedonian history, spanning archaeology, ethnology, anthropology, mythology, and religion from antiquity to the present.
+              Through these pages, they celebrate their teacher's lasting influence and honor her lifelong devotion to truth, scholarship, and her beloved homeland, <strong>Macedonia</strong>.
+            </p>
+          </div>
+        </div>
 
         {/* Fund Call-to-Action */}
-        <CardPair
-          img={"/images/logo-text.jpg"}
-          alt={"Nade Proeva Endowment Fund Logo"}
-          text={
-            <>
-            Your support of the <i>Nade Proeva Endowment Fund</i> will help inspire and empower the next generation
-            of historians dedicated to researching, preserving, and sharing Macedonian historical past. To help grow the Endowment contact{' '}
-            <a href="mailto:info@macedonianarts.org" className="text-red-600 hover:underline">
-              info@macedonianarts.org
-            </a>
-            .
-            </>
-          }
-          imgClass={"py-5"}
-        />
+        <div className="mx-auto w-full xl:w-2/3 flex flex-col items-center sm:block">
+          <img
+            className={`w-[250px] md:self-stretch rounded-lg object-contain sm:ml-5 sm:float-right`}
+            style={{shapeOutside: 'polygon(36% 0, 27% 12%, 29% 24%, 9% 35%, 6% 51%, 12% 70%, 31% 100%, 44% 100%, 100% 0)'}}
+            src="/images/logo-text.jpg"
+            alt="Nade Proeva Endowment Fund Logo"
+            draggable={false}
+          />
+          <div className="text-black text-justify sm:text-right mx-auto p-2">
+            <p className="lg:text-xl">
+              Your support of the <i>Nade Proeva Endowment Fund</i> will help inspire and empower the next generation
+              of historians dedicated to researching, preserving, and sharing Macedonian historical past. To help grow the Endowment contact{' '}
+              <a href="mailto:info@macedonianarts.org" className="text-red-600 hover:underline">
+                info@macedonianarts.org
+              </a>
+              .
+            </p>
+          </div>
+        </div>
       </section>
     </div>
   );
